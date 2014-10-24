58616e5efc67f577d88f30f4e528ef584e89cafe
kushiGIt <kushihataryousuke@yahoo.co.jp>
2014-10-25 00:49:49 +0900

    Add to RKCollectionViewLargeLayout

---
+++ RKCollectionViewLargeLayout.h
@@ -0,0 +1,13 @@
+//
+//  RKCollectionViewLargeLayout.h
+//  RKtimeLineCollectionView
+//
+//  Created by RyousukeKushihata on 2014/10/25.
+//  Copyright (c) 2014年 RyousukeKushihata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RKCollectionViewLargeLayout : UICollectionViewFlowLayout
+
+@end
