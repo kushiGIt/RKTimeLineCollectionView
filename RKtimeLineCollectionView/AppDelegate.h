0ac41fba95c27887489658bc4462abdb8232e1f4
kushiGIt <kushihataryousuke@yahoo.co.jp>
2014-10-25 00:29:29 +0900

    initial commit

---
+++ RKtimeLineCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RKtimeLineCollectionView
+//
+//  Created by RyousukeKushihata on 2014/10/25.
+//  Copyright (c) 2014年 RyousukeKushihata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RKtimeLineCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RKtimeLineCollectionView
+//
+//  Created by RyousukeKushihata on 2014/10/25.
+//  Copyright (c) 2014年 RyousukeKushihata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
